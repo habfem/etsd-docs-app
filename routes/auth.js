8c43840aa07d1c186d7599249f2ecfe02c44139b
Habeeb Olufemi Mustapha M <72679550+habfem@users.noreply.github.com>
2022-11-07 03:26:16 -0600

    logout bug

--- routes/auth.js
+++ routes/auth.js
@@ -4,19 +4,25 @@ const router = express.Router();
 
 // @desc Auth with Google
 // @route GET /auth/google
-router.get ('/google', passport.authenticate('google', { scope: ['profile']}))
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc Google auth callack
 // @route GET /auth/google/callack
-router.get ('/google/callback', passport.authenticate('google', { failureRedirect: '/'}), (req, res) => {
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
   res.redirect('/dashboard')
 })
 
 // @desc Logout user
 //@desc /auth/logout
-router.get('/logout', (req, res)=>{
+/* router.get('/logout', (req, res)=>{
   req.logout()
   res.redirect('/')
+}) */
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/')
+  })
 })
 
 module.exports = router
