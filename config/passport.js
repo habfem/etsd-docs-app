ee5004b4e625455dda46876fc609392872e9d563
Habeeb Olufemi Mustapha M <72679550+habfem@users.noreply.github.com>
2023-07-06 09:28:48 -0500

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,5 @@
+// Outlook strategy
+// Outlook oauth2.0
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const User = require('../models/User');
